94475b666a9acc44907507f46b7f8299ac15a55c
Josh van Niekerk <j.niekerk@students.rave.ac.uk>
2016-12-01 23:32:29 +0000

    working copy of app

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ var detailsSection = $('#details')
 var detailsBackButton = $('#details .back')
 var detailsInfo = $('#details #info')
 
+// define results list outside of any function
+var resultsList = []
 
 // tell the button to do something when we click it
 homeGoButton.click(function(){
@@ -25,14 +27,24 @@ resultsBackButton.click(function(){
     homeSection.show()
 })
 
-
-homeGoButton.click( function(){
-    var chosenOption = homeDropdown.val()
+    
+    // capture the user chosen option
+    homeGoButton.click( function(){
+     var chosenOption = homeDropdown.val()
     console.log("you picked" + chosenOption)
     
-    var resultsList = filterAndSortList(peopleList, chosenOption)
-    showList(resultsList, resultsOL)
-                
+    // filter+sort people by user selection
+    resultsList = filterAndSortList(peopleList, chosenOption);
+    console.log(resultsList);
+        homeSection.hide()
+    resultsSection.show()
+    function showDetails (data, interfaceElement) 
+{
+  var detailsHTML = makeDetailsHTML(data)
+  interfaceElement.html(detailsHTML)
+}
+        showList(resultsList, resultsOL);
+    addMarkers(resultsList);
     $('#results li').click( function() {
         // grab the id from the clicked item
         var resultId = $(this).attr('id')
@@ -45,50 +57,60 @@ homeGoButton.click( function(){
         resultsSection.hide()
         detailsSection.show()
     })
-                
-                homeSection.hide()
-                resultsSection.show()
-})
 
-resultsBackButton.click( function() {
+})    
+
+    
+
+resultsBackButton.click( function(){
     resultsSection.hide()
     homeSection.show()
-} )
+    
+})
 
-
-detailsBackButton.click( function() {
+detailsBackButton.click( function(){
     detailsSection.hide()
     resultsSection.show()
-} )
+    
+})
 
+
+
+// button to switch between list ad map
 resultsToggleButton.click( function() {
-    // find out which element is currently visible
-    // is the list visible?
-    var listDsiplay = resultsOL.css('display')
-    if (listDsiplay == 'block') isListVisible = true
-    else isListVisible = false
     
-    if (isListVisible)
-        {
-            // we want to show the map and hide `the list
-            resultsMap.show()
-            map.resize() // get the map to take all the available space.
-            resultsOL.hide()
-            document.getElementById("toggle").innerHTML = "List"
-            
-        }
+    console.log('clicked resultsToggleButton')
+    
+    // find out which element is currently visable
+    // is the list visable
+    var listDisplay = resultsOL.css('display') 
+    if (listDisplay == 'block') isListVisable = true
+    else isListVisable = false
+    
+    console.log(isListVisable)
+    
+    console.log(listDisplay)
+    // if the list is visable
+    if (isListVisable)
+    {
+       // we want to show the map and hide the list
+        resultsMap.show()
+        map.resize()
+        resultsOL.hide()
+        resultsToggleButton.html('List')
+        // change the button text to say "list"
+        
+    }
     else
-        {
-            // we want to show the list
-            
-            resultsOL.show()
-            resultsMap.hide()
-            document.getElementById("toggle").innerHTML = "Map"
-        }
+    {
+      // we want to show the list and hide the map
+        resultsOL.show()
+        resultsMap.hide()
+        resultsToggleButton.html('Map')
+        // change the button text to say "Map"
+        
+        
+    }
     
-    // if the list is visible, we want to show the map
-    
-    // if the list is hidden, we want to show the list
-    
-}
-)
+   
+})
